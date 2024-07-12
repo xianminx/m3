c2600a006e97824a6ce5d843f0a719eb841c0556
Lucas <xianminx@gmail.com>
2024-07-12 12:39:13 +0800

    Add function to render PNG from HTML content

--- src/utils/mm.ts
+++ src/utils/mm.ts
@@ -23,6 +23,48 @@ export async function renderHtml(markdown: string) {
 }
 
 export async function renderPng(markdown: string): Promise<Buffer> {
+  const html = await renderHtml(markdown);
+  const chromeArgs = [
+    ...chromium.args,
+    // @sparticuz/chromium has default chromeArgs to improve serverless performance, but you can add others as you deem appropriate
+    '--font-render-hinting=none', // Improves font-rendering quality and spacing
+  ];
+
+  // Set this variable as required - @sparticuz/chromium does not work on ARM, so we use a standard Chrome executable locally - see issue https://github.com/Sparticuz/chromium/issues/186
+  const isLocal = false;
+
+  console.log('renderPng chrome path:', await chromium.executablePath());
+
+  if (!browser?.connected) {
+    // If you don't need webGL, this skips the extraction of the bin/swiftshader.tar.br file, improving performance
+    chromium.setGraphicsMode = false;
+    browser = await puppeteer.launch({
+      ...(isLocal
+        ? { channel: 'chrome' }
+        : {
+            args: chromeArgs,
+            executablePath: await chromium.executablePath(),
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true,
+          }),
+    });
+  }
+
+  const page = await browser.newPage();
+
+  await page.setViewport({ width: 1280, height: 720, deviceScaleFactor: 2 });
+  await page.setContent(html);
+
+  const screenshot = await page.screenshot({ fullPage: true });
+  // We don't want headless Chrome instances left running locally
+  if (isLocal) {
+    await browser.close();
+  }
+
+  return screenshot;
+}
+
+export async function renderPng2(markdown: string): Promise<Buffer> {
   // TODO: cache the rendered png file.
   console.time('renderPng');
   let html = await renderHtml(markdown);
@@ -73,6 +115,10 @@ export async function renderPng(markdown: string): Promise<Buffer> {
   // }
 
   console.timeLog('renderPng', 'await browser.newPage');
+  for (const page of await browser.pages()) {
+    await page.close();
+  }
+  // await browser.close();
   const page = await browser.newPage();
   console.timeLog('renderPng', 'page created isLocal=', isLocal);
 
