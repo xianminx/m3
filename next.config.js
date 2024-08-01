d7b1bfbe54b0fe21638601e2564d068a619717a5
Lucas <xianminx@gmail.com>
2024-08-01 17:49:15 +0800

    feat: redirect root to /mind/interactive

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@ import NextBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+          // Basic redirect
+          {
+            source: '/',
+            destination: '/mind/interactive',
+            permanent: true,
+          }
+        ]
+      },
     experimental: {
         serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
     },
