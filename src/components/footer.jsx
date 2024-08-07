937ad1d4249dabf9d797f27de5928434c69d9564
Lucas <xianminx@gmail.com>
2024-08-07 20:18:41 +0800

    Add new logo to navbar and footer components

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,11 @@
 import { Button } from '@nextui-org/button';
+import { Image } from '@nextui-org/react';
 export default function Footer() {
     return (
         <footer className="pt-24">
             <div className="max-w-screen-xl mx-auto px-4 md:px-8">
                 <div className="flex items-center py-10 justify-center">
-                    <img src="/s15n.svg" alt="Logo" className="h-10 w-10" />
+                    <Image src="/logo/s15n.png" alt="Logo" className="h-6 w-6" />
                     <div>© 2024 s15n. All rights reserved. </div>
                     <a href="/terms" target="_blank">
                         <Button variant="light">Terms & policies</Button>
