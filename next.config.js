fb97e2790b1fec71292894ca49ce10bb9d9f7fea
Lucas <xianminx@gmail.com>
2024-08-13 22:39:35 +0800

    style(next.config.js): remove commented out code for basic redirect

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ import NextBundleAnalyzer from '@next/bundle-analyzer';
 const nextConfig = {
     async redirects() {
         return [
-          // Basic redirect
-          // {
-          //   source: '/',
-          //   destination: '/mind/interactive',
-          //   permanent: true,
-          // }
         ]
       },
     experimental: {
